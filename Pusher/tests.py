89f00e7f273497ce9b76806289563711f7fb701c
albertie <3litac@gmail.com>
2019-08-05 22:03:28 +0400

    Added "PROBLEMS.md" and "tests.py"

---
+++ Pusher/tests.py
@@ -0,0 +1,19 @@
+from Pusher import xml_parser
+import requests
+
+xml_link = 'https://www.sports.ru/rss/rubric.xml?s=208'
+
+with open('example.xml', 'w') as file:
+    file.write(requests.get(xml_link).text)
+
+with open('example.xml', 'r') as file:
+    p = xml_parser.ParserXml(file)
+    p.parse_xml()
+    posts = p.get_posts()
+    count = 0
+    for post in posts:
+        print(count, ")")
+        count += 1
+        print(post.get('title'))
+        print(post.get('link'))
+
