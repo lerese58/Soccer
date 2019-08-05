84a94bf9600eb5455c8d9a3ad5e86f29aed1947e
albertie <3litac@gmail.com>
2019-08-05 18:53:54 +0400

    Initial Commit

---
+++ Pusher/utils.py
@@ -0,0 +1,25 @@
+import re
+import requests
+
+
+pattern = r'<div class="news-item__tags-line"> Теги (.*)</div>'
+patton = r'.*?title="(.*?)"'
+
+
+def get_tags_from_page(url):
+
+    """
+    :return: if url.status_code == 200, returns list of lowercase tags
+             else returns an empty list
+    """
+
+    page = requests.get(url)
+    if page.status_code == 200:
+        tags_div = re.findall(pattern, page.text)[0]
+        tags = re.findall(patton, tags_div)
+        tags_lower = []
+        for tag in tags:
+            tags_lower.append(tag.lower())
+        return tags_lower
+    else:
+        return list()
