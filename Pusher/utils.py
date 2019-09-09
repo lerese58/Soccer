1841e7562d6aee9f5c0b6054d8a02e9c223762ab
albertie <3litac@gmail.com>
2019-09-10 01:29:59 +0300

    Lil refactoring in Pusher.utils

--- Pusher/utils.py
+++ Pusher/utils.py
@@ -5,8 +5,8 @@ import requests
 import json
 
 
-def download_xml():
-    with open('../latest_posts.xml', 'w') as file:
+def download_xml(xml_file='../latest_posts.xml'):
+    with open(xml_file, 'w') as file:
         page = requests.get(const.XML_LINK)
         file.write(page.text)
         print(f"xml downloaded with code {page.status_code}")
@@ -18,11 +18,11 @@ def make_json(obj, path):
         json.dump(obj, file, ensure_ascii=False)
 
 
-def get_tagged_posts_from_xml():
+def get_tagged_posts_from_xml(xml_file='../latest_posts.xml'):
     """
     :return: posts_list; each post already has all the tags as an attribute
     """
-    with open('../latest_posts.xml', 'r') as file:
+    with open(xml_file, 'r') as file:
         parser = xml_parser.ParserXml(file)
         posts = parser.get_posts()
         print("parsed")
@@ -40,12 +40,12 @@ def get_tags_from_page(url):
     """
     div_pattern = r'<div class="news-item__tags-line"> Теги (.*)</div>'
     tag_name_pattern = r'.*?title="(.*?)"'
-    print(f'start getting tags from {url}')
+    print(f'starting getting tags from {url}')
     page = requests.get(url)
     tags_div = re.findall(div_pattern, page.text)[0]
     tags = re.findall(tag_name_pattern, tags_div)
     tags_lower = []
     for tag in tags:
         tags_lower.append(tag.lower())
-    print(f'finish getting tags from {url}')
+    print(f'finished getting tags from {url}')
     return tags_lower
