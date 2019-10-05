c19942e7afe63c378f54217b85631e5344b5273d
albertie <3litac@gmail.com>
2019-10-05 23:34:15 +0300

    Merge branch 'master' of /Users/albert/PycharmProjects/Soccer with conflicts.

--- Pusher/test/utils_test.py
+++ Pusher/test/utils_test.py
@@ -1,11 +1,12 @@
 from Pusher import xml_parser
-from settings import XML_LINK, LATEST_POSTS_XML_PATH
+import settings
+
 import requests
 
 if __name__ == '__main__':
-    with open(LATEST_POSTS_XML_PATH, 'w') as file:
-        file.write(requests.get(XML_LINK).text)
-    with open(LATEST_POSTS_XML_PATH, 'r') as file:
+    with open('../latest_posts.xml', 'w') as file:
+        file.write(requests.get(settings.XML_LINK).text)
+    with open('../latest_posts.xml', 'r') as file:
         parser = xml_parser.ParserXml(file)
         posts = parser.get_posts()
         count = 0
